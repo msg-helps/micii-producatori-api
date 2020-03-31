304b2598bf128ee1ec1a896fb076d81570887078
Ors Danalizi-Eross <Ors.Danalizi-Eross@nexontis.com>
2020-03-31 19:07:04 +0300

    Enable strict type checking

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-export const hello = async (event) => {
+export const hello = async (event: any) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
